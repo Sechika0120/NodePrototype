c029b6e6dc4edbcbc23b52d70ce5d4751b8eda2f
岡崎 良祐 <51029082+okazaru@users.noreply.github.com>
2020-07-19 14:46:44 +0900

    Update group.blade.php

--- Prototype/resources/views/group.blade.php
+++ Prototype/resources/views/group.blade.php
@@ -10,7 +10,7 @@
                     <div class="alert alert-success" role="alert">
                             グループへようこそ！！
                         </div>
-                    こんにちは
+                   
 
 
                    
